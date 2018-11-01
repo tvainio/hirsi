2efdc3fb676da9eb6aa00d5e7f791ced7f9edb2b
Teemu Vainio <tvainiotheprogrammer@gmail.com>
2018-11-01 19:07:59 +0200

    Enabled main app smoke test

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,6 @@ import React from "react";
 import { shallow } from "enzyme";
 import App from "./App";
 
-it.skip("renders without crashing", () => {
+it("renders without crashing", () => {
   shallow(<App />);
 });
