cf23a727666e4fc65b4ceeff7f83f7cca2696c3d
Teemu Vainio <tvainiotheprogrammer@gmail.com>
2018-11-04 21:43:25 +0200

    Changed PoorFellow component to a functional component

--- src/components/PoorFellow.jsx
+++ src/components/PoorFellow.jsx
@@ -2,24 +2,18 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Panel } from "react-bootstrap";
 
-class PoorFellow extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-  render() {
-    return (
-      <div>
-        <Panel>
-          <Panel.Body>
-            Poor Fellow! Always in trouble...
-            <div id="health">health {this.props.health}</div>
-          </Panel.Body>
-        </Panel>
-      </div>
-    );
-  }
-}
+const PoorFellow = props => {
+  return (
+    <div>
+      <Panel>
+        <Panel.Body>
+          Poor Fellow! Always in trouble...
+          <div id="health">health {props.health}</div>
+        </Panel.Body>
+      </Panel>
+    </div>
+  );
+};
 
 PoorFellow.propTypes = {
   health: PropTypes.number.isRequired
