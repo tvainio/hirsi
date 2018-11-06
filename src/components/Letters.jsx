76497f82469e82fd9d9ffc7743805b89a74dee77
Teemu Vainio <tvainiotheprogrammer@gmail.com>
2018-11-06 12:43:59 +0200

    some styling to letters

--- src/components/Letters.jsx
+++ src/components/Letters.jsx
@@ -2,22 +2,28 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Panel, Label } from "react-bootstrap";
 
+const letterStyle = {
+  marginLeft: "1px"
+};
+
 function Letters(props) {
   return (
     <div>
       <Panel>
         <Panel.Body>
           <h1>
-            {props.word
-              .split("")
-              .map(
-                (letter, i) =>
-                  props.usedLetters.includes(letter) ? (
-                    <Label key={i}>{letter}</Label>
-                  ) : (
-                    <Label key={i}> </Label>
-                  )
-              )}
+            {props.word.split("").map(
+              (letter, i) =>
+                props.usedLetters.includes(letter) ? (
+                  <Label key={i} style={letterStyle}>
+                    {letter}
+                  </Label>
+                ) : (
+                  <Label key={i} style={letterStyle}>
+                    {" "}
+                  </Label>
+                )
+            )}
           </h1>
         </Panel.Body>
       </Panel>
